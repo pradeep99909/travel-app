e9adfff28523f427f20408a71bcf4e7412bb9d19
Pradeep <pradeep99909@gmail.com>
2019-08-05 22:57:12 +0530

    slider

---
+++ src/component/slider/slider.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Slider extends React.Component{
+    render(){
+        return(
+            <div className="slider">
+                <img alt="Nature" src="https://source.unsplash.com/1600x900/?india,nature,travel" />
+            </div>
+        )
+    }
+}
+
+export default Slider;
+
