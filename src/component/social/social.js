f7e0bb70fb8ddbd03d9ebb07df654ddbc95a62a2
Pradeep Kshirsagar <30392189+pradeep99909@users.noreply.github.com>
2019-09-01 14:15:52 +0530

    Update social.js

--- src/component/social/social.js
+++ src/component/social/social.js
@@ -7,7 +7,7 @@ class Social extends React.Component{
     render(){
         return (
             <div style ={{backgroundColor:this.props.color}} className="social">
-                <a href="#" onClick={(e)=>e.preventDefault()} className={this.props.name}></a>
+                <a onClick={(e)=>e.preventDefault()} className={this.props.name}></a>
                 <div style ={{fontFamily:"sans-serif",backgroundColor:this.props.color}} className="social-text">
                     Login using {this.props.s_text}
                 </div>
